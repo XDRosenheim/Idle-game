7d8d435a8b374b135af5e305be3563249bb0b0a1
XDRosenheim <patrickrosenheim@gmail.com>
2015-10-30 14:05:10 +0100

    First init.

---
+++ Idle game/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Idle_game {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.btnManuelClick = new System.Windows.Forms.Button();
+            this.lblCoins = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.upgrade1 = new System.Windows.Forms.Timer(this.components);
+            this.btnUpgOne = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnManuelClick
+            // 
+            this.btnManuelClick.Location = new System.Drawing.Point(12, 65);
+            this.btnManuelClick.Name = "btnManuelClick";
+            this.btnManuelClick.Size = new System.Drawing.Size(75, 23);
+            this.btnManuelClick.TabIndex = 0;
+            this.btnManuelClick.Text = "Click";
+            this.btnManuelClick.UseVisualStyleBackColor = true;
+            this.btnManuelClick.Click += new System.EventHandler(this.btnManuelClick_Click);
+            // 
+            // lblCoins
+            // 
+            this.lblCoins.AutoSize = true;
+            this.lblCoins.Location = new System.Drawing.Point(29, 29);
+            this.lblCoins.Margin = new System.Windows.Forms.Padding(20);
+            this.lblCoins.Name = "lblCoins";
+            this.lblCoins.Size = new System.Drawing.Size(35, 13);
+            this.lblCoins.TabIndex = 1;
+            this.lblCoins.Text = "label1";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // upgrade1
+            // 
+            this.upgrade1.Tick += new System.EventHandler(this.upgrade1_Tick);
+            // 
+            // btnUpgOne
+            // 
+            this.btnUpgOne.Enabled = false;
+            this.btnUpgOne.Location = new System.Drawing.Point(12, 94);
+            this.btnUpgOne.Name = "btnUpgOne";
+            this.btnUpgOne.Size = new System.Drawing.Size(75, 23);
+            this.btnUpgOne.TabIndex = 2;
+            this.btnUpgOne.Text = "Upgrade 1";
+            this.btnUpgOne.UseVisualStyleBackColor = true;
+            this.btnUpgOne.Click += new System.EventHandler(this.btnUpgOne_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 242);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpgOne);
+            this.Controls.Add(this.lblCoins);
+            this.Controls.Add(this.btnManuelClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnManuelClick;
+        private System.Windows.Forms.Label lblCoins;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer upgrade1;
+        private System.Windows.Forms.Button btnUpgOne;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Idle game/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Idle_game {
+    public partial class Form1 : Form {
+        private static long _coins;
+        private static int _upgradeOneCost, _upgradeOneInterval, _upgradeOneGive;
+        public Form1() {
+            _coins = 0;
+            InitializeComponent();
+            lblCoins.Text = _coins.ToString();
+            _upgradeOneCost = 10;
+            _upgradeOneInterval = 1000;
+            _upgradeOneGive = 1;
+
+        }
+
+        private void btnManuelClick_Click(object sender, EventArgs e) {
+            _coins += 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            lblCoins.Text = _coins.ToString("N0");
+            btnUpgOne.Enabled = _upgradeOneCost <= _coins;
+
+        }
+
+        private void upgrade1_Tick(object sender, EventArgs e) {
+            _coins += _upgradeOneGive;
+        }
+
+        private void btnUpgOne_Click(object sender, EventArgs e) {
+            _coins -= _upgradeOneCost;
+            if (!upgrade1.Enabled) {
+                upgrade1.Enabled = true;
+            }
+            label1.Text = _upgradeOneInterval.ToString();
+            if (_upgradeOneInterval >= 50) {
+                upgrade1.Interval = _upgradeOneInterval;
+                if (_upgradeOneInterval - 50 >= 0)
+                    _upgradeOneInterval = _upgradeOneInterval - 50;
+                else _upgradeOneInterval = 1;
+            }
+            _upgradeOneGive += 1;
+            _upgradeOneCost += 10;
+        }
+    }
+}
